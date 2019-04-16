1b8959cdc2316c24937d843357efdb2b324455f6
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2019-04-16 17:24:46 +0530

    Add files via upload

---
+++ kalimux.sh
@@ -0,0 +1,133 @@
+#kalimux by N17R0 (noob hackers)
+
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+echo " "
+echo " $red
+    ██╗  ██╗ █████╗ ██╗     ██╗███╗   ███╗██╗   ██╗██╗  ██╗
+    ██║ ██╔╝██╔══██╗██║     ██║████╗ ████║██║   ██║╚██╗██╔╝
+    █████╔╝ ███████║██║     ██║██╔████╔██║██║   ██║ ╚███╔╝
+    ██╔═██╗ ██╔══██║██║     ██║██║╚██╔╝██║██║   ██║ ██╔██╗
+    ██║  ██╗██║  ██║███████╗██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗
+    ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝
+                                                       v 1.0 "
+
+echo "$yellow                +~+~+~+~+~+~+~+~+~+~+~+~+~ "
+echo " "
+echo "$green                   <--[Coded by N17R0]-->"
+
+echo "$cyan                ************************** "
+echo "$green              <~~(youtube.com/noobhackers)~~>"
+echo " "
+echo "$yellow                +~+~+~+~+~+~+~+~+~+~+~+~+~ "
+sleep 3.50
+echo " "
+echo  $p 'Please Wait ===+['$b'./                   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'....\                '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'...../               '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'......./             '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........\            '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........../          '$p']\'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........./           '$p']|'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...........\         '$p']/'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']\'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'................/    '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|'
+sleep 0.4
+clear
+
+echo  "$yellow+++++++++++>$p[Please Wait]$yellow<+++++++++++"
+sleep 0.4
+echo " "
+cd $HOME
+
+apt-get update -y
+pkg install wget -y
+pkg install python -y
+pkg install python2 -y
+pkg install openssh -y
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+
+ls
+
+mkdir kali
+
+ls
+
+mv kali-binds kali
+mv kali-fs kali
+mv kali.sh kali
+mv start-kali.sh kali
+
+ls
+
+cd kalimux
+
+ls
+
+cp dekal.txt $HOME
+
+ls
+
+cd $HOME
+
+ls
+
+rm -rf kalimux
+
+ls
+
+mv dekal.txt kali
+
+ls
+
+clear
+
+cd kali
+
+ls
+
+cat dekal.txt
+
+echo "$p------->$yellow [installation completed] $p<--------"
