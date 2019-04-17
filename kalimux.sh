a0c6b0b2b9a649c1e25ec183e1e836df0333cc63
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2019-04-17 14:56:43 +0530

    Update kalimux.sh

--- kalimux.sh
+++ kalimux.sh
@@ -113,7 +113,7 @@ cat dekal.txt
 echo " "
 echo "$p------->$yellow [installation completed] $p<--------"
 echo " "
-sleep 5.10
+sleep 6.10
 echo "$p------->$yellow [Kali Linux is starting wait...] $p<--------"
 echo " "
 sleep 4.30
